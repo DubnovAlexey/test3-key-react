cc2079cb8e3bb851cfc2a564c8fb6ca1a482fdf5
Alexey Dubnov <alex4972@mail.ru>
2025-12-07 16:02:22 +0200

    props 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import UserName from './components/UserName.jsx';
 
 const USERS = [
   {id: 1, name: 'Alex', surname: 'Burn', age: 32, passport: 2902},
-  {id: 2, name: 'Alex', surname: 'Turn', age: 44, passport: 2332},
-  {id: 3, name: 'Berlin', surname: 'Lugan', age: 62, passport: 2350},
-  {id: 4, name: 'Crimson', surname: 'Crimson', age: 12, passport: 7842},
+  // {id: 2, name: 'Alex', surname: 'Turn', age: 44, passport: 2332},
+  // {id: 3, name: 'Berlin', surname: 'Lugan', age: 62, passport: 2350},
+  // {id: 4, name: 'Crimson', surname: 'Crimson', age: 12, passport: 7842},
 ];
 
 
