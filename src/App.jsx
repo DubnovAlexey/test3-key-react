9d8ee212d7e692d7fd2d8417ced51247a8d1e4b7
Alexey Dubnov <alex4972@mail.ru>
2025-12-07 17:31:46 +0200

    onClick 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
-import UserName from './components/UserName.jsx';
-import Button from './components/Button.js';
-import Description from './components/Description.js';
-
-let USERS = [
-  {id: 1, name: 'Alex', surname: 'Burn', age: 32, passport: 2902},
-  {id: 2, name: 'Alex', surname: 'Turn', age: 44, passport: 2332},
-  {id: 3, name: 'Berlin', surname: 'Lugan', age: 62, passport: 2350},
-  {id: 4, name: 'Crimson', surname: 'Crimson', age: 12, passport: 7842},
-];
-
-
 const App = () => {
-  const changeName = (id, newName) => {
-    USERS = USERS.map((user) => {
-      if(user.id === id) {
-        return {...user, name: newName};
-
-      }
-      return user;
-    })
-
-  };
+  const onClickHandler = () => {
+    console.log('clicked');
+  }
   return (
-    <div>
-      {USERS.map((user) => (
-        <div key={(user.id)}>
-          <UserName
-            userName = {user.name}
-            userSurname = {user.surname}
-            UserAge = {user.age}
-            UserPassport={user.passport}
-            changeName = {changeName}
-
-          />
-
-        </div>
-       ))}
+    <div onClick={onClickHandler}>
+      test
     </div>
   )
 }
