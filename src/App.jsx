49c52ad703ea3ca255491c618680d980b8199105
Alexey Dubnov <alex4972@mail.ru>
2025-12-07 16:42:46 +0200

    props.users

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import UserName from './components/UserName.jsx';
 
 const USERS = [
   {id: 1, name: 'Alex', surname: 'Burn', age: 32, passport: 2902},
-  // {id: 2, name: 'Alex', surname: 'Turn', age: 44, passport: 2332},
-  // {id: 3, name: 'Berlin', surname: 'Lugan', age: 62, passport: 2350},
-  // {id: 4, name: 'Crimson', surname: 'Crimson', age: 12, passport: 7842},
+  {id: 2, name: 'Alex', surname: 'Turn', age: 44, passport: 2332},
+  {id: 3, name: 'Berlin', surname: 'Lugan', age: 62, passport: 2350},
+  {id: 4, name: 'Crimson', surname: 'Crimson', age: 12, passport: 7842},
 ];
 
 
@@ -20,15 +20,6 @@ const App = () => {
             UserAge = {user.age}
             UserPassport={user.passport}
 
-            // test2 = {user.passport}
-            // test3 = {4332}
-            // test4 = {'sass'}
-            // test5 = {[user.passport, user.age]}
-            // test6 = {[444,'999', [true], {hello: 'world'}]}
-            // test7 = {false}
-            // test8 = {() =>{}}
-            // test9 = 'tron'
-            // test10
           />
 
         </div>
--- src/components/UserName.jsx
+++ src/components/UserName.jsx
@@ -1,10 +1,20 @@
+
 const UserName = (props) => {
   console.log(props);
   return (
-      <div>
-       Gap
-      </div>
+
+    props.userName >= 20 ? (
+      <p className='oops'>
+        {props.userName}
+        <button>Изменить имя</button>
+      </p>
+  ):
+  (
+    <p className='oops'>{props.userName} {props.userSurname}</p>
+  )
   )
 }
 
-export default UserName;
+
+
+  export default UserName;
